dda5d7ba4c137cf9a3230188891b3faf3ce484ed
wan <wanchunglin.eed06@g2.nctu.edu.tw>
2021-08-09 20:59:58 +0800

    add foreground notification

--- app/src/main/java/NYCU/epidemic/lifeScience/MainActivity.java
+++ app/src/main/java/NYCU/epidemic/lifeScience/MainActivity.java
@@ -1,6 +1,9 @@
 package NYCU.epidemic.lifeScience;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = "default_notification_channel_id";
+            String channelName = "default_notification_channel_name";
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
 //        final FirebaseApp firebaseApp = FirebaseApp.initializeApp(this);
 //        FirebaseMessaging.getInstance().getToken()
 //                .addOnCompleteListener(new OnCompleteListener<String>() {
--- app/src/main/java/NYCU/epidemic/lifeScience/MyFirebaseInstanceIDService.java
+++ app/src/main/java/NYCU/epidemic/lifeScience/MyFirebaseInstanceIDService.java
@@ -1,9 +1,21 @@
 package NYCU.epidemic.lifeScience;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import NYCU.R;
 
 public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
 
@@ -15,12 +27,44 @@ public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
             Log.i("MyFirebaseService","title "+remoteMessage.getNotification().getTitle());
             Log.i("MyFirebaseService","body "+remoteMessage.getNotification().getBody());
         }
-
+        sendNotification(remoteMessage.getNotification().getTitle(),
+                remoteMessage.getNotification().getBody());
     }
 
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
         Log.i("MyFirebaseService","token "+s);
+    }
+
+    private void sendNotification(String messageTitle,String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "default_notification_channel_id";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setContentTitle(messageTitle)
+                        .setSmallIcon(R.drawable.mask)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+//                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
